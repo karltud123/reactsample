8f52e558023efa790f9ec132f548e66ff4d920ca
SPHINET\KARLLIN <karllin@sph.com.sg>
2018-05-12 00:39:41 +0800

    first react set

---
+++ myfirstreact/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Karl", age: 26 },
+      { name: "Michael", age: 16 },
+      { name: "Lin", age: 33 },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+}
+
+export default App;
